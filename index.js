03a9558ff70c3eefd75fd12c6092fd488524dfd1
Jake Turner <10009752+Jakeeyturner@users.noreply.github.com>
2020-10-01 11:07:41 +0100

    Initial smart contract
    Signed-off-by: Jake Turner <10009752+Jakeeyturner@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: 
+ */
+
+'use strict';
+
+const MyAssetContract = require('./lib/my-asset-contract');
+
+module.exports.MyAssetContract = MyAssetContract;
+module.exports.contracts = [ MyAssetContract ];
